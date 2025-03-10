6aace3e92eb6f05f23a347569a1adb84498b6410
psuuzu <sup1@student.unimelb.edu.au>
2025-03-11 01:49:15 +1100

    stuff
    asdf

---
+++ components/ui/nav.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+        <section className="h-[40px] flex justify-centerS align-middle w-full">
+            <p>hey</p>
+            <p>hey</p>
+        </section>
+    )
+  }
+  
