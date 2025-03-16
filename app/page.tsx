402702744cbdafe8eb3e32e3c365f7cf1ff69397
psuuzu <sup1@student.unimelb.edu.au>
2025-03-16 12:06:34 +1100

    text size

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,10 @@ export default function Home() {
     {/* title */}
       <div className="h-[50vh] sm:h-[45vh] md:h-[40vh] lg:h-[35vh] min-w-full flex justify-center items-center">
         <div className="w-[40vw] sm:w-[30vw] md:w-[30vw] flex">
-         <h1 className='text-[30px] sm:text-[40px] md:text-[50px] text-right pr-1'>Welcome to Paul's Portfolio</h1>
+         <h1 className='text-[40px] md:text-[50px] text-right pr-1'>Welcome to Paul's Portfolio</h1>
         </div>
         <div className='w-[40vw] sm:w-[20vw] md:w-[20vw] lg:w-[18vw] flex h-auto'>
-         <p className="text-[12px] sm:text-[14px] md:text-[16px] pl-1">Hi, I am a  UX/UI designer who has an interest in front end development. I enjoy creating user-centered digital solutions</p>
+         <p className="text-[14px] md:text-[16px] pl-1">Hi, I am a  UX/UI designer who has an interest in front end development. I enjoy creating user-centered digital solutions</p>
         </div>
         {/* space above animation / title */}
       </div> 
@@ -34,11 +34,11 @@ export default function Home() {
 
       <section className='flex justify-center'>
         <div className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-auto'>
-          <h2 className='text-[18px] sm:text-[20px] md:text-[30px] mt-[60px]'>Javascript Animation</h2>
-          <p className="text-[14px] sm:text-[16px] md:text-[18px] text-gray-500 mb-[15px]">Front End | Personal Project</p>
-          <p className="text-[12px] sm:text-[14px] md:text-[16px]">Mixing pixel art and animation mechanics with vanila javascript, html and css. a project that was fueled through boredom and some free time</p>
+          <h2 className='text-[20px] md:text-[30px] mt-[60px]'>Javascript Animation</h2>
+          <p className="text-[16px] md:text-[18px] text-gray-500 mb-[15px]">Front End | Personal Project</p>
+          <p className="text-[14px] md:text-[16px]">Mixing pixel art and animation mechanics with vanila javascript, html and css. a project that was fueled through boredom and some free time</p>
           <div className="flex justify-center">
-          <Button variant="outline" className="mt-[25px] hover:bg-black hover:text-white"><p className="text-[12px] sm:text-[14px] md:text-[16px]">view more</p></Button>
+          <Button variant="outline" className="mt-[25px] hover:bg-black hover:text-white"><p className="text-[14px] md:text-[16px]">view more</p></Button>
           </div>
           <Line></Line>
           <Surrounds></Surrounds>
--- components/dsc.tsx
+++ components/dsc.tsx
@@ -4,12 +4,12 @@ export default function Dsc() {
     return (
         <section className="flex flex-col sm:flex-row justify-center items-center w-full mt-[30px]">
             <div className="w-[90vw] sm:w-[35vw] md:w-[30vw] lg:w-[25vw]">
-                <h2 className='text-[18px] sm:text-[20px] md:text-[30px]'>DSCubed IT projects</h2>
-                <p className="text-[14px] sm:text-[16px] md:text-[18px] text-gray-500 mb-[15px]">UI Design | Front End</p>
-                <p className="text-[12px] sm:text-[14px] md:text-[16px]">SurroundSound is a project that won first place and accessibility award in the 2024 CISSA Catalyst Hackathon. Our team developed a working application that uses AI to curate personalized Spotify 
+                <h2 className='text-[20px] md:text-[30px]'>DSCubed IT projects</h2>
+                <p className="text-[16px] md:text-[18px] text-gray-500 mb-[15px]">UI Design | Front End</p>
+                <p className="text-[14px] md:text-[16px]">SurroundSound is a project that won first place and accessibility award in the 2024 CISSA Catalyst Hackathon. Our team developed a working application that uses AI to curate personalized Spotify 
                 playlists that fit the ambiance of user's surroundings, selected mood, and listening history.</p>
                 <div className="flex justify-center sm:justify-end">
-                    <Button variant="outline" className="mt-[25px] hover:bg-black hover:text-white"><p className="text-[12px] sm:text-[14px] md:text-[16px]">view more</p></Button>
+                    <Button variant="outline" className="mt-[25px] hover:bg-black hover:text-white"><p className="text-[14px] md:text-[16px]">view more</p></Button>
                 </div>
             </div>
             <div className="flex items-center">
