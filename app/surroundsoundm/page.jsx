5005500067b41ce9c7eed9ae04ebaa73114a6b7e
psuuzu <sup1@student.unimelb.edu.au>
2025-03-19 12:58:22 +1100

    new page

--- app/surroundsoundm/page.jsx
+++ app/surroundsoundm/page.jsx
@@ -7,11 +7,13 @@ export default function Surroundsoundm() {
 
 
     return (
-        <section className='flex justify-center'>
-            <div className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-auto'>
-                <div className='flex justify-center mt-[40px]'>
-                    <img src="/images/ssphonesl.jpg" alt="gif" className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw]'/>
-                </div>    
+        <>
+        <div className="h-[40px]"></div>
+        <div className='flex justify-center py-[20px]'>
+            <video src="/videos/ssvid.mp4"className="h-[70vh]" autoPlay muted loop/>
+        </div> 
+        <section className='flex justify-center bg-[#1e1e1e]'>
+            <div className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-auto'>   
                 <h2 className='text-[26px] md:text-[30px] mt-[20px]'>SurroundSound</h2>
                 <h3 className="mb-[15px]">UI Design | Hackathon | 2024</h3>
                 <p className="text-[16px]">SurroundSound is a project that won first place and accessibility award in the 2024 CISSA Catalyst Hackathon. Our team developed a working application that uses AI to curate personalized Spotify 
@@ -24,5 +26,7 @@ export default function Surroundsoundm() {
                 </div>
             </div>
         </section>
+        </>
+
     )
 }
