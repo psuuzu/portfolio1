b8b032c0e26416a0a08a7b968deeb797f752f8bc
psuuzu <sup1@student.unimelb.edu.au>
2025-03-22 12:01:15 +1100

    Update page.jsx

--- app/surroundsoundm/page.jsx
+++ app/surroundsoundm/page.jsx
@@ -1,4 +1,5 @@
 
+import { Button } from "@/components/ui/button";
 export default function Surroundsoundm() {
 
 
@@ -18,53 +19,52 @@ export default function Surroundsoundm() {
         </section> 
         <section className='flex justify-center bg-[#1e1e1e]'>
             <div className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-auto'> 
-                <div className="h-[20px]"></div>  
+                <div className="h-[20px]"></div>
                 <p>
-                    SurroundSound is a project that won first place and accessibility award in the 2024 CISSA Catalyst Hackathon. Our team developed a working application that uses AI to curate personalized Spotify 
-                playlists that fit the ambiance of user's surroundings, selected mood, and listening history.
+                     SurroundSound is a project that won first place and accessibility award in the 2024 CISSA Catalyst Hackathon. Our team developed a working application that uses AI to curate personalized Spotify 
+                    playlists that fit the ambiance of user's surroundings, selected mood, and listening history.
                 </p>
-                <div className="flex items-center mt-[15px] ">
-                    <p className="mr-[10px]">Try the app :</p>
-                    <a href="https://surround-sound.vercel.app/" target="_blank" className="break-words flex flex-grow">
-                        <p className="underline">
-                            Click here 
-                        </p>
-                    </a>
-                </div>
-                <div className="flex items-center mt-[15px]">
-                    <p className="mr-[10px]">Github repo :</p>
-                    <a href="https://github.com/micha31r/surround-sound" target="_blank" className="break-words flex flex-grow">
-                        <p className="underline">
-                        Click here 
-                        </p>
-                    </a>
-                </div>
-                <div className="flex items-center mt-[15px]">
-                    <p className="mr-[10px]">Devpost :</p>
-                    <a href="https://devpost.com/software/surroundsound-ydkz0b" target="_blank" className="break-words flex flex-grow">
-                        <p className="underline">
-                        Click here 
-                        </p>
-                    </a>
-                </div>
-                <h2 className='!text-[24px] mt-[20px]'>Role</h2>
+                <section className="flex w-full py-[20px]">
+                    <div className="w-[50%] flex flex-col items-center justify-center h-auto">
+                       
+                            <a href="https://surround-sound.vercel.app/" target="_blank" className="w-full flex justify-center">
+                                <Button variant="outline" className=" hover:bg-[#1e1e1e] hover:border hover:border-input text-black hover:text-white sm:w-[50%]"><h4>Try the App</h4></Button>
+                            </a>
+                            <a href="https://devpost.com/software/surroundsound-ydkz0b" target="_blank" className="w-full flex justify-center mt-[20px]">
+                                <Button variant="outline" className=" hover:bg-[#1e1e1e] hover:border hover:border-input text-black hover:text-white sm:w-[50%]"><h4>Devpost</h4></Button>
+                            </a>
+                            
+                    </div>
+                    <div className="w-[50%] border-l-1 border-[rgb(200,200,200)] mx-[5px] px-[10px]">
+                        <h2 className='!text-[20px]'>
+                            Contribution
+                        </h2>
+                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Market Research</h3>
+                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">User flow and app structure</h3>
+                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Sketching</h3>
+                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Wireframing and prototyping</h3>
+                    </div>
+                    
+                </section>  
+                
+                <h2 className='!text-[24px]'>Role</h2>
                 <p>
                 I Designed an immersive user experience, where users can generate playlists through a single snapshot, explore playlists in a gallery view as a visual diary, and seamlessly save curated playlists to their Spotify profile for centralized access. 
                 </p>
                 <h2 className='!text-[24px] mt-[20px]'>Ideation</h2>
                 <p>
-                Music is associated with vibes and atmospheres. We all have music we love to listen to different occasions like gym music for working out. Currently, it is very hard to explore music that specifically fits one occasion as well as your own music preferences. Instead of manually thinking about what music to pick for an occasion, why not let AI do the guesswork? 
+                Music is deeply tied to atmosphere, but discovering the right songs for specific moments can be tedious. Instead of manually picking music for an occasion, why not let AI do the guesswork? 
                 </p>
                 <h2 className='!text-[24px] mt-[20px]'>Market Research</h2>
                 <p>
-                The initial phase of the project involved conducting research on existing social media and music platforms to identify popular design elements that worked well. This helped pinpoint features such as interactive social sharing and music discovery. Insights from this phase informed the design direction, ensuring that the platform would balance usability, aesthetics, and innovation while catering to user expectations.
+                We analyzed leading music and social platforms to identify popular design elements that worked well. This helped pinpoint features such as interactive social sharing and music discovery. Insights from this phase informed the design direction, ensuring that the platform would balance usability, aesthetics, and innovation while catering to user expectations.
                 </p>
                 <div className="flex justify-center mt-[20px]">
                     <img src="/images/ssfigma.png" alt="surroundsound figma"></img>
                 </div>
                 <h2 className='!text-[24px] mt-[20px]'>Wireframing and prototyping</h2>
                 <p>
-                As the hackathon is only a few days long, the UX design process came under significant pressure. Rapid prototyping was necessary to enable the front-end development to begin as soon as possible. Clear communication between different team roles was crucial in creating a prototype that prioritized efficiency and core functionalities.
+                Due to the short hackathon timeframe, the UX design process came under significant pressure. Rapid prototyping was necessary to enable the front-end development to begin as soon as possible. Clear communication between different team roles was crucial in creating a prototype that prioritized efficiency and core functionalities.
                 </p>
                 <h2 className='!text-[24px] mt-[20px]'>Results</h2>
                 <p>
