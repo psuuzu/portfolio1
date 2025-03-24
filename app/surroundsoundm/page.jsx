21a74a638c8701e2a047defb5da5c4022d06ef41
psuuzu <sup1@student.unimelb.edu.au>
2025-03-24 23:07:52 +1100

    update

--- app/surroundsoundm/page.jsx
+++ app/surroundsoundm/page.jsx
@@ -6,12 +6,12 @@ export default function Surroundsoundm() {
     return (
         <>
         
-        <section className='flex justify-center items-center h-[500px] bg-black'>
+        <section className='flex flex-col sm:flex-row sm:justify-center items-center h-[550px] sm:h-[500px] bg-black'>
             <div> 
-                <img src="/images/surroundsoundlogo.png" alt="surroundsound logo" className="sm:w-[300px] md:w-[400px]"/>
+                <img src="/images/surroundsoundlogo.png" alt="surroundsound logo" className="w-[300px] md:w-[400px]"/>
             </div>
             <div className=" bg-black rounded-xl border-t border-l-[2px] border-b border-[#787878]">
-            <video src="/videos/ssvid.mp4"className="h-[400px] my-[15px] mx-[3px]" autoPlay muted loop/>
+            <video src="/videos/ssvid.mp4"className="h-[350px] sm:h-[400px] my-[15px] mx-[3px]" autoPlay muted loop/>
             </div>
         </section> 
         <section className='flex justify-center bg-[#1e1e1e]'>
@@ -36,7 +36,7 @@ export default function Surroundsoundm() {
                         <h2 className='!text-[20px]'>
                             Contribution
                         </h2>
-                        <h3 className="mb-[15px] !text-[16px] !c">Market Research</h3>
+                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Market Research</h3>
                         <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">User flow and app structure</h3>
                         <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Sketching</h3>
                         <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Wireframing and prototyping</h3>
@@ -52,9 +52,50 @@ export default function Surroundsoundm() {
                 <p>
                 Music is deeply tied to atmosphere, but discovering the right songs for specific moments can be tedious. Instead of manually picking music for an occasion, why not let AI do the guesswork? 
                 </p>
+                <section className="flex flex-col items-center sm:flex-row sm:justify-evenly mt-[10px]">
+                    <div className="w-[90%] sm:w-[48%] h-auto bg-[#2c2c2c] rounded-xl my-[2%] px-[18px] py-[10px]">
+                        <div className="flex items-center">
+                            <img src="/images/cross.png" alt="cross logo" className="w-[20px] h-[20px] mr-[5px]"/>
+                            <h2 className='!text-[20px] flex-1'>
+                                Problem
+                            </h2>
+                        </div>
+                        <ul className="list-disc px-[18px] space-y-[6px]">
+                            <li>
+                                <p className="!text-gray-300">Discovering music could be time consuming</p>
+                            </li>
+                            <li>
+                                <p className="!text-gray-300">Existing platforms rely on generic recommendations </p>
+                            </li>     
+                            <li>
+                                <p className="!text-gray-300">Playlist generations are not context aware</p>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div className="w-[90%] sm:w-[48%] h-auto bg-[#2c2c2c] rounded-xl my-[2%] px-[18px] py-[10px]">
+                        <div className="flex items-center">
+                            <img src="/images/check.png" alt="Tick logo" className="w-[20px] h-[20px] mr-[5px]"/>
+                            <h2 className='!text-[20px] flex-1'>
+                                Solution
+                            </h2>
+                        </div>
+                        <ul className="list-disc px-[18px] space-y-[6px]">
+                            <li>
+                                <p className="!text-gray-300">Using AI and image recognition to analyze user's surrounding</p>
+                            </li>
+                            <li>
+                                <p className="!text-gray-300">Curate personalized Spotify playlists </p>
+                            </li>     
+                            <li>
+                                <p className="!text-gray-300">Save playlists in an organised archive </p>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
                 <h2 className='!text-[24px] mt-[20px]'>Market Research</h2>
                 <p>
-                We analyzed leading music and social platforms to identify popular design elements that worked well. This helped pinpoint features such as interactive social sharing and music discovery. Insights from this phase informed the design direction, ensuring that the platform would balance usability, aesthetics, and innovation while catering to user expectations.
+                We analyzed leading music and social platforms to identify popular design elements that worked well. This helped pinpoint features that  informed the design direction, ensuring that the platform would balance usability, aesthetics, and innovation while catering to user expectations.
                 </p>
                 <div className="flex justify-center mt-[20px]">
                     <img src="/images/ssfigma.png" alt="surroundsound figma"></img>
