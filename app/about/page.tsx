a441b0e251316bd8d56ec35bbeba8de459c1392c
psuuzu <sup1@student.unimelb.edu.au>
2025-03-27 00:14:33 +1100

    test

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+
+export default function Page(){
+    return(
+        <>
+        {/* <section className="h-[90vh] w-[100vw] flex justify-center items-center">
+            <div className="w-[700px] h-[450px] bg-[#212121] rounded-xl flex">
+                <div className="w-[50%] rounded-l-xl bg-white overflow-hidden">
+                    <img src="/images/surf.jpg" alt="surf pic" className="h-full w-full object-cover"/>
+                </div>
+                <div className="w-[50%]">
+
+                </div>
+            </div>
+            
+        </section> */}
+        <img src="/images/surf.jpg" alt="surf pic" className="h-[50vh] sm:h-[80vh] lg:h-[100vh] w-full object-cover"/>
+        </>
+    )
+}
