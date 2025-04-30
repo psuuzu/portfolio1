dda10df3bf6a53cefaaaaa945dd8314206a2ec65
psuuzu <sup1@student.unimelb.edu.au>
2025-04-30 17:27:42 +1000

    surroundsound update

--- app/javascript/page.tsx
+++ app/javascript/page.tsx
@@ -20,7 +20,7 @@ export default function Page(){
         <section className='flex justify-center bg-[#1e1e1e] mt-[60px]'>
             <div className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-auto'> 
                
-                <h2 className='!text-[24px] mt-[40px]'>Draft</h2>
+                <h2 className='!text-[26px] mt-[40px]'>Draft</h2>
                 <p>
                     The pixel art of the animation is first drafted on Piskel on a 80 by 40 grid. 
                 </p>
@@ -37,7 +37,7 @@ export default function Page(){
                     All frames are put together in sequential order. After previewing and tweaking the speed, a suitable framerate was defined. 
                 </p>
 
-                <h2 className='!text-[24px] mt-[40px]'>Implementation in Javascript</h2>
+                <h2 className='!text-[26px] mt-[40px]'>Implementation in Javascript</h2>
                 <p>
                     Animation is the illusion of movement created by displaying a sequence of static frames quickly. 
                 </p>
@@ -54,13 +54,13 @@ export default function Page(){
                     </li>
                 </ul>
                 <img src="/images/animationconcept.png" alt="animation concept visualised" className="mt-[30px]" />
-                <h2 className='!text-[24px] mt-[40px]'>Takeaways</h2>
+                <h2 className='!text-[26px] mt-[40px]'>Takeaways</h2>
                 <p>
                 the animation works as the Javascript logic follows pixel art animation mechanics. However, the code is inefficient, containing long lines of 2d arrays defining each and every pixel of every frame. It is also not scalable, especially for long animations with higher definition as defining pixels on 2d arrays is a tedious process. 
                 </p>
-                <h2 className='!text-[24px] mt-[40px]'>Next Steps</h2>
+                <h2 className='!text-[26px] mt-[40px]'>Next Steps</h2>
                 <p>
-                A program could be developed that reads Piskel files and turns them into JavaScript arrays automatically
+                A program could be developed that reads Piskel files and turns them into JavaScript arrays automatically.
                 </p>
                 <div className="mt-[80px]"></div>
             </div>
--- app/surroundsoundm/page.jsx
+++ app/surroundsoundm/page.jsx
@@ -7,57 +7,44 @@ export default function Surroundsoundm() {
     return (
         <>
         
-        <section className='flex flex-col sm:flex-row sm:justify-center items-center h-[550px] sm:h-[100vh] bg-black'>
-            <div> 
-                <img src="/images/surroundsoundlogo.png" alt="surroundsound logo" className="w-[300px] md:w-[400px]"/>
+        <section className='flex flex-col sm:flex-row sm:justify-center items-center h-[620px] sm:h-[100vh] bg-black'>
+            <div className="w-[300px] md:w-[400px]"> 
+                <img src="/images/surroundsoundlogo.png" alt="surroundsound logo" />
+                <div className="sm:flex justify-end h-auto mx-[10px] mt-[20px] md:mt-0 "> 
+                    
+                <div className="flex justify-center h-auto mx-[10px]">
+                       
+                       <a href="https://surround-sound.vercel.app/" target="_blank" className="flex justify-center mx-[10px]">
+                           <Button variant="outline" className=" hover:bg-[#1e1e1e] hover:border hover:border-input text-[#1e1e1e] hover:text-white "><h4>Try the App</h4></Button>
+                       </a>
+                       <a href="https://devpost.com/software/surroundsound-ydkz0b" target="_blank" className="flex justify-center mx-[10px]">
+                           <Button variant="outline" className=" hover:bg-[#1e1e1e] hover:border hover:border-input text-[#1e1e1e] hover:text-white"><h4>Devpost</h4></Button>
+                       </a>
+                      
+                </div>
+               </div>
             </div>
-            <div className=" bg-black rounded-xl border-t border-l-[2px] border-b border-[#787878]">
+            <div className=" bg-black rounded-xl border-t border-l-[2px] border-b border-[#787878] mt-[30px] sm:mt-0">
             <video src="/videos/ssvid.mp4"className="h-[350px] sm:h-[400px] my-[15px] mx-[3px]" autoPlay muted loop/>
             </div>
+            
         </section> 
         <section className='flex justify-center bg-[#1e1e1e]'>
             <div className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-auto'> 
                 <div className="h-[40px]"></div>
-                <p>
-                     SurroundSound is a project that won first place and accessibility award in the 2024 CISSA Catalyst Hackathon. Our team developed a working application that uses AI to curate personalized Spotify 
-                    playlists that fit the ambiance of user's surroundings, selected mood, and listening history.
+                <p className="!text-[20px] !text-white">
+                SurroundSound is a music platform that uses AI to curate personalized Spotify playlists tailored to the user's environment, mood, and listening history. 
                 </p>
-                <section className="flex w-full py-[20px]">
-                    <div className="w-[50%] flex flex-col items-center justify-center h-auto">
-                       
-                            <a href="https://surround-sound.vercel.app/" target="_blank" className="w-full flex justify-center">
-                                <Button variant="outline" className=" hover:bg-[#1e1e1e] hover:border hover:border-input text-[#1e1e1e] hover:text-white sm:w-[50%]"><h4>Try the App</h4></Button>
-                            </a>
-                            <a href="https://devpost.com/software/surroundsound-ydkz0b" target="_blank" className="w-full flex justify-center mt-[20px]">
-                                <Button variant="outline" className=" hover:bg-[#1e1e1e] hover:border hover:border-input text-[#1e1e1e] hover:text-white sm:w-[50%]"><h4>Devpost</h4></Button>
-                            </a>
-                            
-                    </div>
-                    <div className="w-[50%] border-l-1 border-[rgb(200,200,200)] mx-[5px] px-[10px]">
-                        <h2 className='!text-[20px]'>
-                            Contribution
-                        </h2>
-                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Market Research</h3>
-                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">User flow and app structure</h3>
-                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Sketching</h3>
-                        <h3 className="mb-[15px] !text-[16px] !text-[rgb(200,200,200)]">Wireframing and prototyping</h3>
-                    </div>
-                    
-                </section>  
+                <p className="mt-[20px]">
+                As the sole UI designer, I facilitated the end to end design process—research, Ideation, wireframing and prototyping
+                </p>
+
                 
-                <h2 className='!text-[24px]'>Role</h2>
-                <p>
-                I Designed an immersive user experience, where users can generate playlists through a single snapshot, explore playlists in a gallery view as a visual diary, and seamlessly save curated playlists to their Spotify profile for centralized access. 
-                </p>
-                <h2 className='!text-[24px] mt-[40px]'>Ideation</h2>
-                <p>
-                Music is deeply tied to atmosphere, but discovering the right songs for specific moments can be tedious. Instead of manually picking music for an occasion, why not let AI do the guesswork? 
-                </p>
-                <section className="flex flex-col items-center sm:flex-row sm:justify-evenly mt-[10px]">
-                    <div className="w-[90%] sm:w-[48%] h-auto bg-[#2c2c2c] rounded-xl my-[2%] px-[18px] py-[10px]">
+                <section className="flex flex-col items-center sm:flex-row sm:justify-evenly mt-[20px]">
+                    <div className="w-[90%] sm:w-[48%] h-auto rounded-xl my-[2%] px-[18px] py-[10px]">
                         <div className="flex items-center">
                             <img src="/images/cross.png" alt="cross logo" className="w-[20px] h-[20px] mr-[5px]"/>
-                            <h2 className='!text-[20px] flex-1'>
+                            <h2 className='!text-[26px] flex-1'>
                                 Problem
                             </h2>
                         </div>
@@ -74,10 +61,10 @@ export default function Surroundsoundm() {
                         </ul>
                         
                     </div>
-                    <div className="w-[90%] sm:w-[48%] h-auto bg-[#2c2c2c] rounded-xl my-[2%] px-[18px] py-[10px]">
+                    <div className="w-[90%] sm:w-[48%] h-auto rounded-xl my-[2%] px-[18px] py-[10px]">
                         <div className="flex items-center">
                             <img src="/images/check.png" alt="Tick logo" className="w-[20px] h-[20px] mr-[5px]"/>
-                            <h2 className='!text-[20px] flex-1'>
+                            <h2 className='!text-[26px] flex-1'>
                                 Solution
                             </h2>
                         </div>
@@ -94,22 +81,16 @@ export default function Surroundsoundm() {
                         </ul>
                     </div>
                 </section>
-                <h2 className='!text-[24px] mt-[40px]'>Market Research</h2>
-                <p>
-                We analyzed leading music and social platforms to identify popular design elements that worked well. This helped pinpoint features that  informed the design direction, ensuring that the platform would balance usability, aesthetics, and innovation while catering to user expectations.
-                </p>
-                <div className="flex justify-center mt-[20px]">
-                    <img src="/images/ssmarket.png" alt="market analysis" className="w-[280px] sm:w-[350px]"></img>
-                </div>
-             
-                <h2 className='!text-[24px] mt-[40px]'>Wireframing and prototyping</h2>
+        
+                <h2 className='!text-[26px] mt-[40px]'>Ideation</h2>
+                <h2 className='!text-[26px] mt-[40px]'>Wireframing and prototyping</h2>
                 <p>
                 Due to the short hackathon timeframe, the UX design process came under significant pressure. Rapid prototyping was necessary to enable the front-end development to begin as soon as possible. Clear communication between different team roles was crucial in creating a prototype that prioritized efficiency and core functionalities.
                 </p>
                 <div className="flex justify-center mt-[20px]">
                     <img src="/images/ssfigma.png" alt="surroundsound figma"></img>
                 </div>
-                <h2 className='!text-[24px] mt-[40px]'>Results</h2>
+                <h2 className='!text-[26px] mt-[40px]'>Results</h2>
                 <p>
                 Our team managed to obtain First place amongst 78 other participants. My UI design was praised by the judge for its effective mobile design and implementation, earning us another accessibility award, bringing the team a total of $850 in cash prize.
                 </p>
