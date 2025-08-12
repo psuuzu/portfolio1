152164d9bb6ff8a73f249c351bf9b47863fb8a6a
psuuzu <sup1@student.unimelb.edu.au>
2025-08-13 00:20:09 +1000

    test

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -29,19 +29,38 @@ export default function Page() {
   }, []);
 
   return (
-    <div className=" bg-black flex justify-center">
-      <div className="mt-[60px] flex w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-[620px] sm:h-[90vh] justify-between">
-        <div
-          className={`w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px]  transition-colors duration-75 ${visible[0] ? "bg-white" : "bg-black"}`}
-        />
-        <div
-          className={`w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px] transition-colors duration-75 ${visible[1] ? "bg-white" : "bg-black"}`}
-        />
-        <div
-          className={`w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px] transition-colors duration-75 ${visible[2] ? "bg-white" : "bg-black"}`}
-        />
+    <section>
+    <div className="relative bg-black flex justify-center mt-[60px]">
+      <div className="flex w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] justify-between relative z-10">
+        <div className="relative w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px]">
+          <div className={`w-full h-full ${visible[0] ? "visible" : "invisible"} relative z-10`}>
+            <div className="absolute bottom-5"><h1 className=" lg:text-[10vw] md:text-[12vw] sm:text-[14vw] text-[18vw]">Paul</h1></div>
+            <img src="/images/surfbio.jpg" alt="banner" className="h-full object-cover object-left" />
+          </div>
+        </div>
+        <div className="relative w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px]">
+          <div className={`w-full h-full ${visible[1] ? "visible" : "invisible"} relative z-10`}>
+            <div className="absolute bottom-5"><h1 className="lg:text-[10vw] md:text-[12vw] sm:text-[14vw] text-[18vw]">Su</h1></div>
+            <img src="/images/surfbio.jpg" alt="banner" className="h-full object-cover object-center" />
+          </div>
+        </div>
+        <div className="relative w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px]">
+          <div className={`w-full h-full ${visible[2] ? "visible" : "invisible"} relative z-10`}>
+            <img src="/images/surfbio.jpg" alt="banner" className="h-full object-cover object-right" />
+          </div>
+        </div>
       </div>
     </div>
+    <div className="justify-center flex w-full">
+       <div className="flex-row w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw]">
+        <p className="mt-[24px]">I’m a UX designer with a background in visual arts, passionate about creating intuitive solutions that are visually appealing and backed by research and testing. Beyond UX design, I am expanding my front end development skills in react to bring my designs to life</p>  
+        <p className="mt-[16px]"> in my free time (if i get any) I enjoy doing visual arts, playing music, and surfing.</p>
+
+      </div>
+    </div>
+     
+    
+    </section>
+    
   );
 }
-
