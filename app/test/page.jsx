bd1295f346c69032e16cb0e15ac1bb5cc436cd91
psuuzu <sup1@student.unimelb.edu.au>
2025-08-13 14:14:38 +1000

    animation

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,66 +1,100 @@
-'use client';
-import React, { useEffect, useState } from "react";
+'use client'
+import React, { useState, useEffect } from "react";
+import Line from "@/components/line";
+import Social from "@/components/ui/social";
+import { motion } from "motion/react"
 
-const flickerPattern = [
-  [0, 1, 0, 1], // Rectangle 1: flicker twice
-  [0, 1, 0, 1, 0, 1], // Rectangle 2: flicker three times
-  [0, 1, 0, 1, 1], // Rectangle 3: flicker with a double white at end
-];
+export default function Page(){
 
-export default function Page() {
-  const [visible, setVisible] = useState([0, 0, 0]);
+    return(
+        <>
+        <section className="h-auto sm:h-[100vh] w-[100vw] flex flex-col justify-center items-center mt-[50px] sm:mt-0">
+            <div className="flex flex-col sm:flex-row items-center sm:justify-center h-[auto] sm:h-[34vw] lg:h-[20vw]">
+                <motion.div
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.8, ease: "easeOut", delay:0.5}}
+                  >
+                    <div className="w-[80vw] sm:w-[42vw] lg:w-[30vw] flex-col hidden sm:block">
+                        <h1 className='text-[40px] md:text-[50px] pr-1'>Paul Su</h1>
+                        <p className="mt-[20px]  pr-[10px]">I’m a UX designer with a background in visual arts, passionate about creating intuitive solutions that are visually appealing and backed by research and testing. Beyond UX design, I am expanding my front end development skills in react to bring my designs to life</p>
+                        <p className="hidden pr-[10px] mt-[16px]"> in my free time (if i get any) I enjoy doing visual arts, playing music, and surfing.</p>
+                    </div>
+                  </motion.div>
+                  {/* mobile title */}
+                  <motion.div
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.8, ease: "easeOut", delay:0.5}}
+                  >
+                    <div className="w-[80vw] sm:w-[42vw] lg:w-[30vw] flex-col block sm:hidden">
+                        <h1 className='text-[40px] md:text-[50px] pr-1'>Paul Su</h1>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                  initial={{ opacity: 1, x: "-15vw" }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.8, ease: "easeOut", delay:0.5 }}
+                  >
+                    <div className="w-[80vw] sm:w-[42vw] lg:w-[30vw] h-[70vw] sm:h-[34vw] lg:h-[20vw] rounded-xl overflow-hidden hidden sm:block">
+                        <img src="/images/surf.jpg" alt="surf pic" className="h-full w-full object-cover scale-150"/>
+                    </div>
+                  </motion.div>
+                  {/* mobile image */}
+                  <motion.div
+                  initial={{ opacity: 1, y: "15vh" }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, ease: "easeOut", delay:0 }}
+                  >
+                  <div className="w-[80vw] sm:w-[42vw] lg:w-[30vw] h-[70vw] sm:h-[34vw] lg:h-[20vw] rounded-xl overflow-hidden sm:hidden">
+                        <img src="/images/surf.jpg" alt="surf pic" className="h-full w-full object-cover scale-150"/>
+                  </div>
+                  </motion.div>
+            </div>
+            
+            <div className="hidden sm:block">
+                <Line></Line>
+                <motion.div
+              initial={{ opacity: 0, y: -80 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, ease: "easeOut", delay:0.5 }}
+            >
+                <div className="w-[80vw] sm:w-[84vw] lg:w-[60vw] flex flex-col sm:flex-row sm:justify-between items-center mt-[40px]">
+                    <div className="flex flex-row items-center mb-[5px] sm:m-0">
+                        <h2 className='text-[20px] md:text-[24px]'>Contacts</h2>
+                        <p className="ml-[20px]">psuuzu@gmail.com</p>
+                    </div>
+                    <Social></Social>
+                </div>
+                </motion.div> 
+            </div> 
+           
+            {/* mobile content */}
+            <motion.div
+                  initial={{ opacity: 0, y: -100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.8, ease: "easeOut", delay:0.5}}
+                  >
+              <div className="w-[80vw] sm:w-[84vw] lg:w-[60vw] flex-col justify-center items-center block sm:hidden">
+                <div className="w-[80vw]">
+                  <p className="mt-[20px] ">I’m a UX designer with a background in visual arts, passionate about creating intuitive solutions that are visually appealing and backed by research and testing. Beyond UX design, I am expanding my front end development skills in react to bring my designs to life</p>
+                  <p className="mt-[16px]"> in my free time (if i get any) I enjoy doing visual arts, playing music, and surfing.</p>
+                </div>
+                <Line></Line>
+                    <div className="flex flex-row items-center justify-center my-[10px]">
+                        <h2 className='text-[20px] md:text-[24px]'>Contacts</h2>
+                        <p className="ml-[20px]">psuuzu@gmail.com</p>
+                    </div>
+                <div className="flex justify-center">
+                  <Social></Social>
+                </div>
+              </div>
+            </motion.div>
+              
+            
+            <div className="mt-[80px] sm:hidden"></div>
+        </section>
 
-  useEffect(() => {
-    // Flicker logic for each rectangle
-    flickerPattern.forEach((pattern, idx) => {
-      let time = 0;
-      pattern.forEach((val, i) => {
-        setTimeout(() => {
-          setVisible((prev) => {
-            const next = [...prev];
-            next[idx] = val;
-            return next;
-          });
-        }, time);
-        // Each flicker is a bit different in speed
-        time += 80 + idx * 40 + Math.floor(Math.random() * 30);
-      });
-    });
-  }, []);
-
-  return (
-    <section>
-    <div className="relative bg-black flex justify-center mt-[60px]">
-      <div className="flex w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] justify-between relative z-10">
-        <div className="relative w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px]">
-          <div className={`w-full h-full ${visible[0] ? "visible" : "invisible"} relative z-10`}>
-            <div className="absolute bottom-5"><h1 className=" lg:text-[10vw] md:text-[12vw] sm:text-[14vw] text-[18vw]">Paul</h1></div>
-            <img src="/images/surfbio.jpg" alt="banner" className="h-full object-cover object-left" />
-          </div>
-        </div>
-        <div className="relative w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px]">
-          <div className={`w-full h-full ${visible[1] ? "visible" : "invisible"} relative z-10`}>
-            <div className="absolute bottom-5"><h1 className="lg:text-[10vw] md:text-[12vw] sm:text-[14vw] text-[18vw]">Su</h1></div>
-            <img src="/images/surfbio.jpg" alt="banner" className="h-full object-cover object-center" />
-          </div>
-        </div>
-        <div className="relative w-[31%] h-[250px] sm:h-[350px] md:h-[400px] lg:h-[450px]">
-          <div className={`w-full h-full ${visible[2] ? "visible" : "invisible"} relative z-10`}>
-            <img src="/images/surfbio.jpg" alt="banner" className="h-full object-cover object-right" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="justify-center flex w-full">
-       <div className="flex-row w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw]">
-        <p className="mt-[24px]">I’m a UX designer with a background in visual arts, passionate about creating intuitive solutions that are visually appealing and backed by research and testing. Beyond UX design, I am expanding my front end development skills in react to bring my designs to life</p>  
-        <p className="mt-[16px]"> in my free time (if i get any) I enjoy doing visual arts, playing music, and surfing.</p>
-
-      </div>
-    </div>
-     
-    
-    </section>
-    
-  );
+        </>
+    )
 }
