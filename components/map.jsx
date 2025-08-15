a8b5fad2fe5136abb7f9565350162f3e867856dd
psuuzu <sup1@student.unimelb.edu.au>
2025-08-16 00:23:07 +1000

    update

--- components/map.jsx
+++ components/map.jsx
@@ -24,14 +24,14 @@ export default function Map({ project, setProject, showSecondDiv , setShowSecond
              
                 <div onClick={()=> setProject("ux")} className="px-[10px]">
                     <div className="flex items-center">
-                        <h2 className="text-[24px]">Route</h2>
+                        <h2 className="text-[26px]">Route</h2>
                         <div className="bg-white rounded-sm px-[5px] mx-[5px]">
-                            <h2 className="text-[20px] !text-black">01</h2>
+                            <h2 className="text-[22px] !text-black">01</h2>
                         </div>
-                        <p className="!text-[18px] ml-[5px]">to UX/UI Design</p>
+                        <p className="!text-[20px] ml-[5px]">UX/UI Design</p>
                     </div>
-                    <p className="!text-[#838383] mt-[5px]">7 min</p>
-                    {/* <img src="/images/route1.png" className="w-[90vw] sm:w-[35vw] md:w-[30vw] lg:w-[25vw] h-auto max-w-[300px] mb-[5px]"/>                                    */}
+                    <p className="!text-[#838383] mt-[5px]">Ideas -------- Prototype</p>
+                    {/* <img src="/images/route1.p  ng" className="w-[90vw] sm:w-[35vw] md:w-[30vw] lg:w-[25vw] h-auto max-w-[300px] mb-[5px]"/>                                    */}
                 </div>
               
                 </motion.div>
@@ -41,26 +41,26 @@ export default function Map({ project, setProject, showSecondDiv , setShowSecond
                 whileTap={{ scale: 0.9 }}
                 > 
                 
-                <div className="mt-[4vh] px-[10px]" onClick={()=> setProject("ui")}>
+                <div className="mt-[8vh] px-[10px]" onClick={()=> setProject("ui")}>
                     <div className="flex items-center">
-                        <h2 className="text-[24px]">Route</h2>
+                        <h2 className="text-[26px]">Route</h2>
                         <div className="bg-white rounded-sm px-[5px] mx-[5px]">
-                            <h2 className="text-[20px] !text-black">02</h2>
+                            <h2 className="text-[22px] !text-black">02</h2>
                         </div>
-                        <p className="!text-[18px] ml-[5px]">to Front-end Interactions</p>
+                        <p className="!text-[20px] ml-[5px]">Front-End</p>
                     </div>
-                    <p className="!text-[#838383] mt-[5px]">14 min</p>
+                    <p className="!text-[#838383] mt-[5px]">Design -------- Interactive Product</p>
                     {/* <img src="/images/route2.png" className="w-[90vw] sm:w-[35vw] md:w-[30vw] lg:w-[25vw] h-auto max-w-[300px] mb-[5px]"/> */}
                 </div>  
                 </motion.div>
                 
-                <div className="mt-[4vh] opacity-60 px-[10px]">
+                <div className="mt-[8vh] opacity-60 px-[10px]">
                     <div className="flex items-center">
-                        <h2 className="text-[24px]">Route</h2>
+                        <h2 className="text-[26px]">Route</h2>
                         <div className="bg-white rounded-sm px-[5px] mx-[5px]">
-                            <h2 className="text-[20px] !text-black">03</h2>
+                            <h2 className="text-[22px] !text-black">03</h2>
                         </div>
-                        <p className="!text-[18px] ml-[5px]">to Creative Works</p>
+                        <p className="!text-[20px] ml-[5px]">Creative Works</p>
                     </div>
                     <p className="mt-[5px]">(Under Construction)</p>
                     {/* image and button goes here         */}
