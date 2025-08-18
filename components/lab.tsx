936d323920c294b24db0e5dc75a894b8d0bd7a65
psuuzu <sup1@student.unimelb.edu.au>
2025-08-18 12:13:04 +1000

    update

--- components/lab.tsx
+++ components/lab.tsx
@@ -11,7 +11,7 @@ export default function Lab() {
             </div>
             <div className="w-[90vw] sm:w-[35vw] md:w-[30vw] lg:w-[25vw] sm:pl-[10px]">
                 <h2 className='text-[26px] md:text-[30px]'>Yummly Usability Evaluation</h2>
-                <h3 className="mb-[15px]"> UX Research | UX testing  </h3>
+                <h3 className="mb-[15px]"> UX Research | UX Testing  </h3>
                 <p className="text-[16px]">This study evaluated the Yummly website using expert and user-based methods to identify usability issues and recommend improvements for clearer navigation, better feedback, and more recognizable interface elements.</p>
                 <div className="flex justify-center sm:justify-start">
                 <Button variant="outline" className="mt-[25px] hover:bg-black hover:border hover:border-input text-black hover:text-white" onClick={()=>router.push("lab")}><h4>View more</h4></Button>
