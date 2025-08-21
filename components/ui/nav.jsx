87f309fa3975171a539205c1beeb411bf96e4c19
psuuzu <sup1@student.unimelb.edu.au>
2025-08-21 23:08:28 +1000

    test

--- components/ui/nav.jsx
+++ components/ui/nav.jsx
@@ -6,8 +6,8 @@ export function Navbar() {
     return (
         
         <section className="h-auto items-center justify-center w-full inline-flex top-0 bg-black" id='top'>
-            <p className=" px-[16px] py[2px] hover:!text-white hover:border-b h-[40px] items-center flex" onClick={() => router.push("/")}>Home</p>
-            <p className=" px-[16px] py[2px] hover:!text-white hover:border-b h-[40px] items-center flex" onClick={() => router.push("about")}>About</p>
+            <p className=" px-[16px] py[2px] hover:!text-white hover:border-b h-[40px] items-center flex cursor-pointer" onClick={() => router.push("/")}>Home</p>
+            <p className=" px-[16px] py[2px] hover:!text-white hover:border-b h-[40px] items-center flex cursor-pointer" onClick={() => router.push("about")}>About</p>
         </section>
     )
 }
