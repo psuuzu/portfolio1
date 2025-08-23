34b0f6765fa73b692df3bc3dcd36553b4d1c3bd5
psuuzu <sup1@student.unimelb.edu.au>
2025-08-23 17:07:54 +1000

    description

--- components/movement.tsx
+++ components/movement.tsx
@@ -145,7 +145,7 @@ export default function Movement() {
       
         <h2 className='text-[26px] md:text-[30px] mt-[20px]'>Programmable Movement</h2>
         <h3 className=" mb-[15px]">Front End | University Coursework </h3>
-        <p className="text-[16px]">This is a Javascript program that allows user to move and program the block around that leaves a trail behind. This project is then integrated into this website by adjusting javascript into typescript rules</p>
+        <p className="text-[16px]">This is a Javascript program that allows user to move and program the block around, leaving a trail behind. This project is then integrated into this website by adjusting javascript into typescript rules</p>
         <div className="flex justify-center">
           <a href="https://github.com/psuuzu/portfolio1/blob/master/components/movement.tsx" target="_blank">
           <Button variant="outline"  className="mt-[25px] hover:bg-black hover:border hover:border-input text-black hover:text-white"><h4>Github</h4></Button>
