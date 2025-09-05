6e3c9c81c0a9599ab798743b46e85a11e60abe4b
psuuzu <sup1@student.unimelb.edu.au>
2025-09-05 11:12:14 +1000

    test

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -3,10 +3,11 @@ import { useState } from 'react';
 import Line from "@/components/line";
 import { Button } from "@/components/ui/button";
 import { button } from 'framer-motion/client';
+import { StickyScrollReveal } from '@/components/creative/stickyscroll';
 
 export default function Test() {
   const [sboriginal, setSboriginal] = useState(false);
-  let starbreak = "/images/creative/sb.png"
+  let starbreak = "/images/creative/sb.jpg"
   let buttontext = "View Original"
 
   function switchImage(){
@@ -15,17 +16,17 @@ export default function Test() {
 
   if(sboriginal==true){
     buttontext = "View Edited"
-    starbreak = "/images/creative/sbo.png"
+    starbreak = "/images/creative/sbo.jpg"
   }
 
   if(sboriginal==false){
     buttontext = "View Original"
-    starbreak = "/images/creative/sb.png"
+    starbreak = "/images/creative/sb.jpg"
   }
 
 
   return (
-    <section className='flex justify-center'>
+    <section className='flex items-center flex-col'>
         <div className='w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] h-auto z-40 bg-black'> 
           <div className="my-[80px] flex-col flex items-center">
             <h1 className='text-[40px] md:text-[50px] '>Creative Works</h1>
@@ -33,12 +34,17 @@ export default function Test() {
           </div> 
           
           
-          <img src="/images/creative/game.jpg" alt="Game poster" className="rounded-xl"/>
+          <img src="/images/creative/gameb.jpg" alt="Game poster" className="rounded-xl"/>
           <h2 className='text-[26px] md:text-[30px] mt-[20px]'>First To The Moon</h2>
           <h3 className="mb-[15px]"> Game Design  </h3>
           <p className="text-[16px]">
             Selected to present as game designer at the Cybernetic Futures Showcase, Melbourne Design Week 2025. Designed game concept and mechanics; conducted playtesting and iterated based on user feedback to refine gameplay
           </p>
+          <div className="flex justify-center mt-[25px]">
+                <a href="https://showcase.technologiesforartisticresearch.com.au/#G15" target="_blank" className="flex justify-center">
+                    <Button variant="outline" className=" hover:bg-black hover:border hover:border-input text-black hover:text-white"><h4>View More</h4></Button>
+                </a> 
+            </div>
           <Line></Line>
           <img src={starbreak} alt="Starbreak Sprite Manipulation" className="mt-[40px] rounded-xl"/>
           <div className='flex justify-between items-center w-full mt-[20px]'>
@@ -55,10 +61,8 @@ export default function Test() {
                 </a> 
             </div>
           <Line></Line>
-          
-
-       
         </div>
+        <StickyScrollReveal></StickyScrollReveal>
       </section>
   );
 }
